7ffd269475c806e742022db82577daeedd202fad
No-impossible <codyyountred11@gmail.com>
2026-04-02 02:04:43 +0530

    WIP: changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import DoctorDashboard from './pages/DoctorDashboard';
+import DoctorLogin from './pages/DoctorLogin';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/doctor/login" element={<DoctorLogin />} />
         <Route path="/doctor" element={<DoctorDashboard />} />
       </Routes>
     </BrowserRouter>
